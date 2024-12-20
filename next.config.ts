56aec9b1370ef3a6602f91aa478b4b9ba9f7df87
Kohut Arsen <kohut.arsen@gmail.com>
2024-12-20 19:59:24 +0200

    deploy changes #10

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
     serverActions: {
       bodySizeLimit: '25mb',
@@ -10,13 +9,23 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://test-task-db2-limited.vercel.app/" }, 
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
+        source: "/api/:path*", 
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://test-task-db2-limited.vercel.app/" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      },
+      {
+        // Додаємо заголовок CSP для дозволу завантаження скриптів з Clerk
+        source: "/:path*",  // Цей заголовок буде застосовуватись до всіх шляхів
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `default-src 'self'; script-src 'self' https://clerk.accounts.dev https://cdn.clerk.com; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;`
+          }
+        ]
       }
     ]
   }
