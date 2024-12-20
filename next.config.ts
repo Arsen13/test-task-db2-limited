64c4a71a5f94fa9fe8fd6a224a24b0b2fb289d4d
Kohut Arsen <kohut.arsen@gmail.com>
2024-12-20 17:48:33 +0200

    deploy changes #7

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '25mb',
-    }
-  },
   api: {
     bodyParser: {
       sizeLimit: '25mb',
